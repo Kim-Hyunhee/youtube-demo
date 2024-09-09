0dc2b3ca19edf82da40af9e2b35e69c17d78823b
Kim-Hyunhee <tta_bi@naver.com>
2024-09-09 11:21:49 +0900

    로그인 기본 로직

--- user-demo.js
+++ user-demo.js
@@ -8,7 +8,28 @@ let db = new Map();
 var id = 1; // 하나의 객체를 유니크하게 구별하기 위함
 
 // 로그인
-app.post("/login", function (req, res) {});
+app.post("/login", function (req, res) {
+  console.log(req.body); // userId, pwd
+
+  // userId가 디비에 저장된 회원인지 확인
+  const { userId, password } = req.body;
+  db.forEach(function (user, id) {
+    // forEach(function (a, b, c))
+    // a: 데이터(value) b: 인덱스(key) c: 객체(Map)
+    // console.log(user.userId);
+    if (user.userId === userId) {
+      console.log("같은 거 찾았다!");
+
+      console.log(password, user.password);
+      // pwd도 맞는지 비교
+      if (user.password === password) {
+        console.log("패스워드도 같다!");
+      } else {
+        console.log("패스워드는 틀렸다!");
+      }
+    }
+  });
+});
 
 // 회원 가입
 app.post("/join", function (req, res) {
