bbb21d2db51207b9ed294f2724c9fb44ff477937
Kim-Hyunhee <tta_bi@naver.com>
2024-09-13 09:34:17 +0900

    json array 비구조화

--- db-demo.js
+++ db-demo.js
@@ -5,12 +5,18 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1234",
+  password: "root",
   database: "Youtube",
+  dateStrings: true,
 });
 
 // simple query
 connection.query("SELECT * FROM `users`", function (err, results, fields) {
-  console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
+  const { id, email, name, created_at } = results[0];
+  console.log(id);
+  console.log(email);
+  console.log(name);
+  console.log(created_at);
+  // console.log(results); // results contains rows returned by server
+  // console.log(fields); // fields contains extra meta data about results, if available
 });
