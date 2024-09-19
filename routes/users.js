c654db0568b9533ad732f086f77d8c89523c5d3a
Kim-Hyunhee <tta_bi@naver.com>
2024-09-19 10:48:18 +0900

    delete,로그인

--- routes/users.js
+++ routes/users.js
@@ -11,24 +11,23 @@ let db = new Map();
 // 로그인
 router.post("/login", function (req, res) {
   // userId, pwd
-
   // userId가 디비에 저장된 회원인지 확인
-  const { userId, password } = req.body;
+  const { email, password } = req.body;
 
-  let loginUser = hasUserId(db, userId);
-
-  if (isExist(loginUser)) {
-    // pwd도 맞는지 비교
-    if (loginUser.password === password) {
-      res
-        .status(200)
-        .json({ message: `${loginUser.name}님 로그인 되었습니다.` });
-    } else {
-      res.status(400).json({ message: `비밀번호가 틀렸습니다.` });
+  conn.query(
+    `SELECT * FROM users WHERE email = ?`,
+    email,
+    function (err, results, fields) {
+      const loginUser = results[0];
+      if (loginUser && loginUser.password == password) {
+        res
+          .status(200)
+          .json({ message: `${loginUser.name}님 로그인 되었습니다.` });
+      } else {
+        res.status(404).json({ message: `이메일 또는 비밀번호가 틀렸습니다.` });
+      }
     }
-  } else {
-    res.status(404).json({ message: `회원 정보가 없습니다.` });
-  }
+  );
 });
 
 function isExist(obj) {
@@ -39,29 +38,16 @@ function isExist(obj) {
   }
 }
 
-function hasUserId(db, userId) {
-  let foundUser = {};
-
-  db.forEach((user) => {
-    if (user.userId === userId) {
-      foundUser = user;
-    }
-  });
-
-  return foundUser || {};
-}
-
 // 회원 가입
 router.post("/join", function (req, res) {
-  console.log(req.body);
   if (req.body == {}) {
     res.status(400).json({ message: `입력 값을 다시 확인해주세요.` });
   } else {
     const { email, name, password, contact } = req.body;
 
     conn.query(
-      `INSERT INTO users (email, name, password, contact )
-      VALUES (?, ?, ?, ?),`,
+      `INSERT INTO users (email, name, password, contact)
+      VALUES (?, ?, ?, ?);`,
       [email, name, password, contact],
       function (err, results, fields) {
         res.status(201).json(results);
@@ -84,16 +70,15 @@ router
     );
   })
   .delete(function (req, res) {
-    let { userId } = req.body;
+    let { email } = req.body;
 
-    const user = db.get(userId);
-    if (user) {
-      db.delete(userId);
-
-      res.status(200).json({ message: `${user.name}님 다음에 또 뵙겠습니다.` });
-    } else {
-      res.status(404).json({ message: "회원 정보가 없습니다." });
-    }
+    conn.query(
+      `DELETE FROM users WHERE email = ?`,
+      email,
+      function (err, results, fields) {
+        res.status(200).json(results);
+      }
+    );
   });
 
 module.exports = router;
